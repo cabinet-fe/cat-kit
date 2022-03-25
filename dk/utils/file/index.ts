6b3fefdf989fb4a7ab7fa2f7d1e6f3ae89830b30
whj <emailwen@qq.com>
2022-03-25 18:28:51 +0800

    chore: file

---
+++ dk/utils/file/index.ts
@@ -0,0 +1,22 @@
+class Filer {
+  private file!: File
+
+  constructor(file: File, chunkSize: number) {
+    this.file = file
+  }
+
+
+  getHash() {
+
+  }
+
+  getChunks() {
+
+  }
+}
+
+export function F() {
+  return
+}
+
+export type { Filer }
