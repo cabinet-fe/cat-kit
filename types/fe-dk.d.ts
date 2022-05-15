6163dd4c491aaf2cf61e7b20f19bd83ff84ccfed
whj <emailwen@qq.com>
2022-05-15 16:33:16 +0800

    fix: 修复http delete方法不能够传请求体的bug

--- types/fe-dk.d.ts
+++ types/fe-dk.d.ts
@@ -372,9 +372,10 @@ declare class Http {
     /**
      * 用于对资源进行删除
      * @param url 请求url
+     * @param data 请求数据
      * @param options 请求选项
      */
-    delete<T>(url: string, options?: AliasRequestConfig): Promise<HttpResponse<T>>;
+    delete<T>(url: string, data: any, options?: AliasRequestConfig): Promise<HttpResponse<T>>;
     /**
      * 用于对资源进行部分修改
      * @param url 请求url
