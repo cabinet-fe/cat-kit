53a3dd28f7a3c64aaf1d6a5b5846f5b437e584d7
whj <emailwen@qq.com>
2022-10-19 10:57:58 +0800

    docs: [ci]

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -62,7 +62,8 @@ const sidebar: DefaultTheme.Sidebar = {
         { text: '基础', link: '/shared/term/basic' },
         { text: 'oauth2', link: '/shared/term/oauth2' },
         { text: 'restful', link: '/shared/term/restful' },
-        { text: '工作流', link: '/shared/term/workflow' }
+        { text: '工作流', link: '/shared/term/workflow' },
+        { text: '元编程', link: '/shared/term/meta-program' }
       ]
     },
     {
