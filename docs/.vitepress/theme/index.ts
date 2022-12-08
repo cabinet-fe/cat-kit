8336a9833d5dd1f1342df7c40a0872b4bca7f4d6
whj <emailwen@qq.com>
2022-12-08 13:51:53 +0800

    docs: [ci] modify docs theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import { Theme } from 'vitepress'
 import defaultTheme from 'vitepress/dist/client/theme-default/index'
 import components from '../components'
 import 'highlight.js/styles/atom-one-dark.css'
+import './theme.css'
 
 const theme: Theme = {
   ...(defaultTheme as Theme),
