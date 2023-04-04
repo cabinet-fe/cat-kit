75ac03d5b2d7f0cf09da82a6abe32e23902c1614
whj <emailwen@qq.com>
2023-04-04 14:15:46 +0800

    chore: chore

--- packages/common/tree/tree.ts
+++ packages/common/tree/tree.ts
@@ -1,66 +1,108 @@
+interface TreeVal {
+  [key: string]: any
+  children?: TreeVal[]
+}
 
+class TreeNode<Val extends TreeVal> {
+  /** 值 */
+  val: Val
 
-class Tree<D> {
-  data!: D
+  /** 父节点 */
+  parent?: TreeNode<Val>
 
-  parent?: Tree<D>
+  /** 子节点 */
+  children?: TreeNode<Val>[]
 
-  leafs = new Set<Tree<D>>()
+  /** 深度 */
+  depth = 1
 
-  depth: number = 1
+  constructor(val: Val, parent?: TreeNode<Val>) {
+    this.val = val
+    if (val.children) {
+      this.children = val.children.map(child => new TreeNode(child, this))
+    }
 
-  children?: Tree<D>[]
-
-  constructor(data: D, parent?: Tree<D>) {
-    this.data = data
     if (parent) {
       this.parent = parent
-    }
-    if (Array.isArray(data)) {
-
     }
   }
 
   /**
-   * 移除某个节点, 传入数据时进行搜索, 不穿则移除自身
-   * @param data 数据
+   * 从当前节点开始深度优先遍历
+   * @param cb 回调
    */
-  remove(data?: D) {
-    if (data) {
-      return this.walk((node) => {
-        if (node.data === data) node.remove()
-      })
+  dfs(cb: (tree: TreeNode<Val>) => void) {
+    cb(this)
+    this.children?.forEach(child => child.dfs(cb))
+  }
+
+  /**
+   * 从当前节点开始广度优先遍历
+   * @param cb 回调
+   */
+  bfs(cb: (tree: TreeNode<Val>) => void) {
+    const queue: TreeNode<Val>[] = [this]
+    while (queue.length > 0) {
+      const node = queue.shift()!
+      cb(node)
+      if (node.children?.length) {
+        queue.push(...node.children)
+      }
     }
+  }
+}
 
-    if (!this.parent) return
+class Tree<V extends TreeVal> {
+  root: TreeNode<V>
 
-    this.parent.children = this.parent.children!.filter(child => child !== this)
+  leafs = new Set<Tree<V>>()
+
+  constructor(root: TreeNode<V>) {
+    this.root = root
+  }
+
+  insert(node: TreeNode<V>, val: V) {
+    const child = new TreeNode(val, node)
+
+    if (node.children) return child
   }
 
   /**
    * 从当前节点开始遍历
    * @param cb 回调
-   * @param type 遍历类型 df深度优先 bf广度优先 back回朔
+   * @param type 遍历类型 dfs深度优先 bfs广度优先
    */
-  walk(cb: (tree: Tree<D>) => void, type: 'df' | 'bf' | 'back' = 'df') {
-
+  walk(cb: (node: TreeNode<V>) => void, type: 'dfs' | 'bfs' = 'dfs') {
+    this.root[type](cb)
   }
 
   /**
-   * 往当前树的子树中插入一条数据
-   * @param data 数据
+   * 移除某个节点, 传入数据时进行搜索, 不穿则移除自身
+   * @param target 要删除的目标节点
    */
-  insert(data: D) {
-    const node = new Tree(data, this)
-    if (this.children) {
-      this.children.push(node)
-    } else {
-      this.children = [node]
+  remove(target: TreeNode<V>) {
+    const { parent } = target
+    if (parent?.children) {
+      parent.children.splice(parent.children.indexOf(target), 1)
     }
   }
-
 }
 
+function createTree<
+  D extends TreeVal[] | TreeVal,
+  V extends TreeVal = D extends Array<any> ? D[number] : D
+>(data: D): Tree<V> {
+  const root = new TreeNode(Array.isArray(data) ? { children: data } : data)
 
+  const tree = new Tree(root)
 
-export default Tree
+  return tree as any
+}
+
+const tree = createTree([{ name: '' }])
+
+tree.walk(node => {
+  node.val.name
+})
+
+export default createTree
