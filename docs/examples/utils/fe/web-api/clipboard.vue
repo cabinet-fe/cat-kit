aa376f45fc76562ee1f01f03f8a440b93d91bb64
whj <emailwen@qq.com>
2023-07-20 09:04:40 +0800

    docs: [ci] release v3.1.6

--- docs/examples/utils/fe/web-api/clipboard.vue
+++ docs/examples/utils/fe/web-api/clipboard.vue
@@ -2,6 +2,8 @@
   <div>
     <input type="text" placeholder="输入" style="border: 1px solid #ccc;" v-model="text" />
     <a style="cursor: pointer;" @click="copy">复制</a>
+
+    <a @click="handleImport">从剪切板中导入</a>
   </div>
 </template>
 
@@ -14,5 +16,10 @@ const text = shallowRef('')
 const copy = async () => {
   await clipboard.copy(text.value)
   alert('复制成功, 粘贴测试')
+}
+
+const handleImport = async () => {
+  const data = await clipboard.readText()
+  alert(`当前导入的内容为: ${data}`)
 }
 </script>
--- packages/fe/web-api/clipboard.ts
+++ packages/fe/web-api/clipboard.ts
@@ -23,6 +23,23 @@ function createClipboardData(
   }
 }
 
+const requestPermission = async (
+  permissionName: PermissionName | 'clipboard-read' | 'clipboard-write'
+) => {
+  // 尝试获取权限(chrome浏览器需要获取权限)
+  const permission = await navigator.permissions
+    // @ts-ignore 类型中不支持clipboard-write
+    .query({ name: permissionName })
+    .catch(() => {
+      // 报错说明不支持该权限, 手动给一个granted
+      return {
+        state: 'granted'
+      }
+    })
+
+  return permission.state !== 'denied'
+}
+
 /** 剪切板 */
 export const clipboard = {
   /**
@@ -30,7 +47,7 @@ export const clipboard = {
    * @param data 写入的数据
    */
   async copy(data: string | Blob | Array<string | Blob>): Promise<void> {
-    const { clipboard, permissions } = navigator
+    const { clipboard } = navigator
 
     if (!clipboard) {
       if (typeof data === 'string') {
@@ -40,8 +57,8 @@ export const clipboard = {
         el.style.width = '0'
         el.style.height = '0'
         el.style.position = 'fixed'
-        el.style.left='0'
-        el.style.top='0'
+        el.style.left = '0'
+        el.style.top = '0'
         el.style.borderWidth = '1px'
         el.style.pointerEvents = 'none'
 
@@ -56,16 +73,9 @@ export const clipboard = {
     }
 
     // 尝试获取权限(chrome浏览器需要获取权限)
-    // @ts-ignore 类型中不支持clipboard-write
-    const permission = await permissions.query({ name: 'clipboard-write' })
-      .catch(() => {
-        // 报错说明不支持该权限, 手动给一个granted
-        return {
-          state: 'granted'
-        }
-      })
+    const hasPermission = await requestPermission('clipboard-write')
 
-    if (permission.state === 'denied') {
+    if (!hasPermission) {
       return Promise.reject('用户未授权复制到剪切板操作')
     }
 
@@ -74,5 +84,59 @@ export const clipboard = {
     }
 
     return clipboard.write([createClipboardData(data)])
+  },
+
+  /**
+   * 从剪切板中读取纯文本数据
+   * @returns 读取到的文本数据
+   */
+  async read() {
+    // 检查浏览器是否支持 Clipboard API，并且是否有读取权限
+    if (!navigator.clipboard?.read) {
+      throw new Error('当前浏览器不支持从剪切板读取数据')
+    }
+    const hasPermission = await requestPermission('clipboard-read')
+
+    if (!hasPermission) {
+      throw new Error('用户未授权读取剪切板内容')
+    }
+
+    try {
+      // 读取剪切板内容
+      const clipboardItems = await navigator.clipboard.read()
+
+      const clipboardItem = clipboardItems[0]
+
+      if (!clipboardItem) {
+        throw new Error('未读取到剪切板内容')
+      }
+
+      // 检查剪切板内容是否为纯文本
+      if (!clipboardItem.types.includes('text/plain')) {
+        throw new Error('剪切板内容不是纯文本')
+      }
+
+      // 获取剪切板文本数据
+      const textData = await clipboardItem.getType('text/plain')
+
+      return textData.text()
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  /**
+   * 读取文本内容
+   * @returns 剪切板中的文本内容
+   */
+  async readText() {
+    if (!navigator.clipboard?.readText) {
+      throw new Error('当前浏览器不支持从剪切板读取数据')
+    }
+    const data = await navigator.clipboard.readText().catch(() => {
+      return Promise.reject('无法读取剪切板内容')
+    })
+
+    return data
   }
 }
