728db003b9817592f44a46d7f56272485ae491ef
whj <emailwen@qq.com>
2023-12-11 18:09:13 +0800

    refactor: 对软件进行部分重构，并且增加了一些新的功能

--- packages/fe/db/db.ts
+++ packages/fe/db/db.ts
@@ -1,141 +1,84 @@
-interface DBOptions {
-  /** 数据库发生错误时的回调 */
-  onError?: (err: any) => void
-  /** 数据库升级后的回调 */
-  onUpgradeneeded?: (db: IDBDatabase) => void
-  /** 版本更改事件 */
-  onVersionchange?: (e: IDBVersionChangeEvent) => void
+interface StoreColumn<T extends any = any> {
+  autoIncrement?: boolean
+  primary?: boolean
+  describe?: string
+  type: T extends Number ? number : T extends String ? string : T extends Boolean ? boolean : T extends Date ? Date : T
 }
 
-/**
- * 连接一个数据库
- * @param name 需要连接的数据库的名称
- * @param version 数据库的版本
- * @param options 选项
- * @returns
- */
-export function openDB(name: string, version?: number, options?: DBOptions) {
-  return new Promise<IDBDatabase['createObjectStore']>((rs, rj) => {
-    const request = indexedDB.open(name, version)
-
-    request.addEventListener('success', () => {})
-
-    request.addEventListener('error', err => {
-      rj(err)
-      options?.onError?.(err)
-    })
-
-    request.addEventListener('upgradeneeded', () => {
-      let db = request.result
-      console.log(Object.values(db.objectStoreNames))
-      function createObjectStore(
-        name: string,
-        options?: IDBObjectStoreParameters
-      ) {
-        if (!db.objectStoreNames.contains(name)) {
-          return db.createObjectStore(name, options)
-        } else {
-          let storeTransaction = db.transaction([name], 'readwrite')
-          let store = storeTransaction.objectStore(name)
-          return store
-        }
-      }
-
-      rs(createObjectStore)
-
-      if (options?.onVersionchange) {
-        db.onversionchange = options?.onVersionchange
-      }
-
-      options?.onUpgradeneeded?.(db)
-    })
-  })
+interface StoreColumns<Keys extends string = string> {
+  [key in Keys]: StoreColumn<any>
 }
 
-/**
- * 数据
- */
-export function deleteDB() {}
+class WebDB {
+  #db?: IDBDatabase
 
+  readonly name: string
 
-// 封装一个基于IndexedDB的api
-interface DatabaseOptions {
-  databaseName: string;
-  storeName: string;
-  keyPath: string;
-}
+  readonly version?: number
 
-export class IndexedDBApi<T> {
-  private readonly options: DatabaseOptions;
+  readonly stores: WebDBStore[] = []
 
-  constructor(options: DatabaseOptions) {
-    this.options = options;
+  async #init() {
+    const request = window.indexedDB.open(this.name, this.version)
+    request.onsuccess = ev => {
+      this.#db = request.result
+    }
+    request.onupgradeneeded = ev => {
+      this.#db = request.result
+    }
   }
 
-  private async openDatabase(): Promise<IDBDatabase> {
-    return new Promise((resolve, reject) => {
-      const request = window.indexedDB.open(this.options.databaseName);
-      request.onerror = () => reject(request.error);
-      request.onsuccess = () => resolve(request.result);
-      request.onupgradeneeded = () => {
-        const database = request.result;
-        database.createObjectStore(this.options.storeName, { keyPath: this.options.keyPath });
-      };
-    });
+  constructor(DBName: string, version?: number) {
+    this.name = DBName
+    this.version = version
+
+    this.#init()
   }
 
-  async getAll(): Promise<T[]> {
-    const db = await this.openDatabase();
-    const transaction = db.transaction(this.options.storeName, 'readonly');
-    const store = transaction.objectStore(this.options.storeName);
-    return new Promise((resolve, reject) => {
-      const request = store.getAll();
-      request.onerror = () => reject(request.error);
-      request.onsuccess = () => resolve(request.result);
-    });
-  }
-
-  async getById(id: number | string): Promise<T> {
-    const db = await this.openDatabase();
-    const transaction = db.transaction(this.options.storeName, 'readonly');
-    const store = transaction.objectStore(this.options.storeName);
-    return new Promise((resolve, reject) => {
-      const request = store.get(id);
-      request.onerror = () => reject(request.error);
-      request.onsuccess = () => resolve(request.result);
-    });
-  }
-
-  async add(item: T): Promise<void> {
-    const db = await this.openDatabase();
-    const transaction = db.transaction(this.options.storeName, 'readwrite');
-    const store = transaction.objectStore(this.options.storeName);
-    return new Promise((resolve, reject) => {
-      const request = store.add(item);
-      request.onerror = () => reject(request.error);
-      request.onsuccess = () => resolve();
-    });
-  }
-
-  async update(id: number | string, item: T): Promise<void> {
-    const db = await this.openDatabase();
-    const transaction = db.transaction(this.options.storeName, 'readwrite');
-    const store = transaction.objectStore(this.options.storeName);
-    return new Promise((resolve, reject) => {
-      const request = store.put(item, id);
-      request.onerror = () => reject(request.error);
-      request.onsuccess = () => resolve();
-    });
-  }
-
-  async delete(id: number | string): Promise<void> {
-    const db = await this.openDatabase();
-    const transaction = db.transaction(this.options.storeName, 'readwrite');
-    const store = transaction.objectStore(this.options.storeName);
-    return new Promise((resolve, reject) => {
-      const request = store.delete(id);
-      request.onerror = () => reject(request.error);
-      request.onsuccess = () => resolve();
-    });
+  initStores(stores: WebDBStore[] | WebDBStore) {
+    if (Array.isArray(stores)) {
+      this.stores.push(...stores)
+    } else {
+      this.stores.push(stores)
+    }
   }
 }
+
+class WebDBStore<Keys extends string = string> {
+  name: string
+  readonly columns: Record<Keys, StoreColumn>
+
+  constructor(name: string, columns: Record<Keys, StoreColumn>) {
+    this.name = name
+    this.columns = columns
+  }
+
+  /**
+   * 创建一个对象
+   * @param object 存储对象
+   */
+  create(object: Record<Keys, any>) {}
+
+  /**
+   * 删除一个对象
+   */
+  delete() {}
+
+  update() {}
+
+  read() {}
+}
+
+const db = new WebDB('aa')
+
+const s = new WebDBStore('test', {
+  id: {
+    autoIncrement: true,
+    primary: true,
+    type: Number
+  }
+})
+
+s.create({
+  id: 1
+})
