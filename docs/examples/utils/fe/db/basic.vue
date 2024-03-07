aa85b7ceb4e47c6a47668c12cb2daa4ad59099ee
whj <emailwen@qq.com>
2024-03-07 16:23:16 +0800

    feat: db封装增量更新

--- docs/examples/utils/fe/db/basic.vue
+++ docs/examples/utils/fe/db/basic.vue
@@ -1,7 +1,23 @@
 <template>
-
+  <div>请在控制台查看效果</div>
 </template>
 
-<script lang="ts" setup>
- import {  } from '@cat-kit/fe'
+<script lang="ts" async setup>
+import { WebDB, defineDBStores } from '@cat-kit/fe'
+
+const stores = defineDBStores({
+  users: {
+    name: { example: '', required: true }
+  }
+})
+
+const db = await WebDB.create({
+  name: 'my_db',
+  stores
+})
+
+db.stores.users.create({ name: '你好' })
+
+db.stores.users.delete()
+db.stores.users.delete()
 </script>
--- packages/fe/db/db.ts
+++ packages/fe/db/db.ts
@@ -11,8 +11,23 @@ interface StoreColumn<T = unknown> {
   required?: boolean
 }
 
-export class WebDB {
-  #db?: IDBDatabase
+type StoresConfigs = {
+  [key: string]: {
+    [key: string]: StoreColumn
+  }
+}
+
+interface DBOptions<Stores extends StoresConfigs> {
+  /** 数据库名称 */
+  name: string
+  /** 数据库版本 */
+  version?: number
+  /** 数据库对象存储库 */
+  stores: Stores
+}
+
+export class WebDB<Stores extends StoresConfigs> {
+  #db: IDBDatabase
 
   readonly name: string
 
@@ -20,32 +35,42 @@ export class WebDB {
 
   #queue: any[] = []
 
-  async #clearQueue() {
-    while (this.#queue.length) {
-      this.#queue.shift()!.db = this.#db
-    }
+  stores!: {
+    [key in keyof Stores]: WebDBStore<string, Stores[key]>
   }
 
-  async #init() {
+  /**
+   * 浏览器数据库
+   * @param DBName 数据库名称
+   * @param version 数据库版本
+   */
+  constructor(options: DBOptions<Stores> & { onOpened: (db: any) => void }) {
+    this.name = options.name
+    this.version = options.version
     const request = window.indexedDB.open(this.name, this.version)
-    request.onsuccess = ev => {
-      this.#db = request.result
-      this.#clearQueue()
-    }
+
+    // request.onsuccess = ev => {
+    //   this.#db = request.result
+    //   this.#clearQueue()
+    // }
     request.onupgradeneeded = ev => {
       this.#db = request.result
       while (this.#queue.length) {
         this.#queue.shift()!.db = this.#db
       }
-      this.#clearQueue()
+
+      const stores = {} as {
+        [key in keyof Stores]: WebDBStore<string, Stores[key]>
+      }
+
+      for (const storeName in options.stores) {
+        stores[storeName] = this.#createStore(storeName, stores[storeName])
+      }
+
+      this.stores = stores
+
+      options.onOpened(this)
     }
-  }
-
-  constructor(DBName: string, version?: number) {
-    this.name = DBName
-    this.version = version
-
-    this.#init()
   }
 
   /**
@@ -54,14 +79,10 @@ export class WebDB {
    * @param columns
    * @returns
    */
-  defineStore<
+  #createStore<
     Name extends string,
     Cols extends Record<string, StoreColumn> = Record<string, StoreColumn>,
-    Obj = {
-      [key in RequiredKeys<Cols>]: Cols[key]['example']
-    } & {
-      [key in keyof Omit<Cols, RequiredKeys<Cols>>]?: Cols[key]['example']
-    }
+    Obj extends GetRecord<Cols> = GetRecord<Cols>
   >(name: Name, columns: Cols) {
     const store = new WebDBStore<Name, Cols, Obj>(name, columns)
 
@@ -73,33 +94,76 @@ export class WebDB {
 
     return store
   }
+
+  /**
+   * 创建数据库
+   */
+  static async create<Stores extends StoresConfigs>(
+    options: DBOptions<Stores>
+  ): Promise<WebDB<Stores>> {
+    return new Promise((rs, rj) => {
+      try {
+        new WebDB({ ...options, onOpened: rs })
+      } catch (error) {
+        rj(error)
+      }
+    })
+  }
 }
 
 type RequiredKeys<Cols extends Record<string, StoreColumn>> = {
   [key in keyof Cols]: Cols[key]['required'] extends true ? key : never
 }[keyof Cols]
 
+type GetRecord<Cols extends Record<string, StoreColumn>> = {
+  [key in RequiredKeys<Cols>]: Cols[key]['example']
+} & {
+  [key in keyof Omit<Cols, RequiredKeys<Cols>>]?: Cols[key]['example']
+}
+
 class WebDBStore<
-  Name extends string = string,
-  Cols extends Record<string, StoreColumn> = Record<string, StoreColumn>,
-  Obj = {
-    [key in RequiredKeys<Cols>]: Cols[key]['example']
-  } & {
-    [key in keyof Omit<Cols, RequiredKeys<Cols>>]?: Cols[key]['example']
-  }
+  Name extends string,
+  Cols extends Record<string, StoreColumn>,
+  Obj extends GetRecord<Cols> = GetRecord<Cols>
 > {
   name: Name
+
   readonly columns: Cols
   tasks: Set<any> = new Set()
   #db?: IDBDatabase
 
+  #store: IDBObjectStore
+
   set db(db: IDBDatabase) {
     this.#db = db
+    this.tasks.forEach(task => task())
+  }
+
+  #createStore(name: Name) {
+    const { db, columns } = this
+    if (!db) return
+
+    const existStore = db.objectStoreNames.contains(name)
+    if (existStore) {
+      return db.transaction(name, 'readwrite').objectStore(name)
+    }
+    const columnKeys = Object.keys(columns)
+    this.#store = db.createObjectStore(this.name, {
+      keyPath: columnKeys.find(key => columns[key].primary)
+    })
   }
 
   constructor(name: Name, columns: Cols) {
     this.name = name
     this.columns = columns
+
+    if (this.#db) {
+      this.#createStore(name)
+    } else {
+      this.tasks.add(() => {
+        this.#createStore(name)
+      })
+    }
   }
 
   /**
@@ -112,6 +176,7 @@ class WebDBStore<
         const store = this.#db!.transaction(this.name, 'readwrite').objectStore(
           this.name
         )
+
         const req = store.add(object)
 
         req.onsuccess = () => {
@@ -143,4 +208,12 @@ class WebDBStore<
   read() {}
 
   count() {}
+}
+
+const users = new WebDBStore('users', {
+  name: { example: 'aa', required: true }
+})
+
+export function defineDBStores<C extends StoresConfigs>(configs: C) {
+  return configs
 }
