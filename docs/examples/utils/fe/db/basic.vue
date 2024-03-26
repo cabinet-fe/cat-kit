5dbca44b86a62ecb74bfdd3340b74551a620e0f5
whj <emailwen@qq.com>
2024-03-26 17:08:37 +0800

    docs: [ci]

--- docs/examples/utils/fe/db/basic.vue
+++ docs/examples/utils/fe/db/basic.vue
@@ -3,21 +3,21 @@
 </template>
 
 <script lang="ts" async setup>
-import { WebDB, defineDBStores } from '@cat-kit/fe'
+// import { WebDB, defineDBStores } from '@cat-kit/fe'
 
-const stores = defineDBStores({
-  users: {
-    name: { example: '', required: true }
-  }
-})
+// const stores = defineDBStores({
+//   users: {
+//     name: { example: '', required: true }
+//   }
+// })
 
-const db = await WebDB.create({
-  name: 'my_db',
-  stores
-})
+// const db = await WebDB.create({
+//   name: 'my_db',
+//   stores
+// })
 
-db.stores.users.create({ name: '你好' })
+// db.stores.users.create({ name: '你好' })
 
-db.stores.users.delete()
-db.stores.users.delete()
+// db.stores.users.delete()
+// db.stores.users.delete()
 </script>
