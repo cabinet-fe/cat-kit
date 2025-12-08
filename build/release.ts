4f5d172d3ad3ea303a49480a6e3e1ff1fb65266c
whj <emailwen@qq.com>
2025-12-08 11:30:25 +0800

    chore: 杂项

---
+++ build/release.ts
@@ -0,0 +1,42 @@
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import { main } from './repo'
+import { $ } from 'execa'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+
+async function test() {
+  const s = await $({
+    cwd: path.resolve(__dirname, '../packages/tests'),
+  })`bun run test`.catch(err => {
+
+    console.log(11111)
+  })
+
+}
+
+async function build() {
+  await main.build({
+    '@cat-kit/be': {
+      platform: 'node'
+    },
+    '@cat-kit/excel': {
+      platform: 'browser'
+    }
+  })
+}
+
+async function release() {
+  await test()
+  await build()
+
+  main.bumpVersion({
+    type: 'premajor'
+  })
+}
+
+
+release()
